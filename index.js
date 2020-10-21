90f550d870b9a295817ca1d045af6bbfdca53b33
Magoo222 <magoo.1.9.2@gmail.com>
2020-10-21 20:18:03 +0100

    Added basic form

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
-    res.send("Hi there!");
+    res.send(`
+    <div>
+        <form method="POST"> 
+            <input name="email" placeholder = "E-mail" />
+            <input name="password" placeholder = "Password" />
+            <input name="passwordconfirmation" placeholder = "Password Confirmation" />
+            <button>Sign Up</button>
+        </form>
+    </div>
+    `);
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send("Account Created!");
 });
 
 app.listen(3000, () => {
